f912f785d73be6868115c071de763594460bc40d
fahma20 <fahmaxo@gmail.com>
2024-10-31 08:49:55 +0300

    reacts assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import About from "./About"; 
+import user from "../data/user"; // Ensure this path is correct
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+      name={user.name} 
+      city={user.city}
+      color={user.color} />
+      <About 
+        bio={user.bio} 
+        github={user.links.github} 
+        linkedin={user.links.linkedin} 
+      />
     </div>
   );
 }
